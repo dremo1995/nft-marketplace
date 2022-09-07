f566749b1927fe0523d856634b4ca0deb8070bed
Andre <asm13august95@gmail.com>
2022-09-07 18:12:41 -0500

    NFT cards

---
+++ components/Button.js
@@ -0,0 +1,28 @@
+import { TouchableOpacity, View, Text, Image } from "react-native";
+import { COLORS, SHADOWS, SIZES } from "../constants";
+
+export const CircleButton = ({ imgURL, handlePress, ...props }) => {
+  return (
+    <TouchableOpacity
+      style={{
+        width: 40,
+        height: 40,
+        backgroundColor: COLORS.white,
+        position: "absolute",
+        borderRadius: SIZES.extraLarge,
+        alignItems: "center",
+        justifyContent: "center",
+        ...SHADOWS.light,
+        ...props,
+      }}
+    ></TouchableOpacity>
+  );
+};
+
+export const RectButton = () => {
+  return (
+    <View>
+      <Text>Button</Text>
+    </View>
+  );
+};
--- screens/Home.js
+++ screens/Home.js
@@ -1,12 +1,39 @@
-import { Text, View, SafeArea, FlatList } from "react-native";
+import { Text, View, SafeAreaView, FlatList } from "react-native";
 import { useState } from "react";
 
 import { COLORS, NFTData } from "../constants";
-import { SafeAreaView } from "react-native-safe-area-context";
 import { NFTCard, HomeHeader, FocusedStatusBar } from "../components";
 
 const Home = () => {
-  return <SafeAreaView style={{ flex: 1 }}></SafeAreaView>;
+  return (
+    <SafeAreaView style={{ flex: 1 }}>
+      <FocusedStatusBar background={COLORS.primary} />
+      <View style={{ flex: 1 }}>
+        <View style={{ zIndex: 0 }}>
+          <FlatList
+            data={NFTData}
+            renderItem={({ item }) => <NFTCard data={item} />}
+            keyExtractor={(item) => item.id}
+            showsVerticalScrollIndicator={false}
+            ListHeaderComponent={<HomeHeader />}
+          />
+        </View>
+        <View
+          style={{
+            position: "absolute",
+            top: 0,
+            bottom: 0,
+            right: 0,
+            left: 0,
+            zIndex: -1,
+          }}
+        >
+          <View style={{ height: 300, backgroundColor: COLORS.primary }} />
+          <View style={{ flex: 1, backgroundColor: COLORS.white }} />
+        </View>
+      </View>
+    </SafeAreaView>
+  );
 };
 
 export default Home;
