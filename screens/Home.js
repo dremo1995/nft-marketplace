fa181e4e53681de8fb1147ccebfb84e0919d53c8
Andre <asm13august95@gmail.com>
2022-09-07 16:15:26 -0500

    Initial commit

---
+++ screens/Home.js
@@ -0,0 +1,12 @@
+import { Text, View, SafeArea, FlatList } from "react-native";
+import { useState } from "react";
+
+import { COLORS, NFTData } from "../constants";
+import { SafeAreaView } from "react-native-safe-area-context";
+import { NFTCard, HomeHeader, FocusedStatusBar } from "../components";
+
+const Home = () => {
+  return <SafeAreaView style={{ flex: 1 }}></SafeAreaView>;
+};
+
+export default Home;
