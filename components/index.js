d86e58d10f1b600d2e95d2fab33b42cfa39814a9
Andre <asm13august95@gmail.com>
2022-09-09 13:10:37 -0500

    Finish home screen and start details screen

--- components/index.js
+++ components/index.js
@@ -2,5 +2,29 @@ import HomeHeader from "./HomeHeader";
 import FocusedStatusBar from "./FocusedStatusBar";
 import NFTCard from "./NFTCard";
 import { CircleButton, RectButton } from "./Button";
+import {
+  SubInfo,
+  EthPrice,
+  NFTTitle,
+  People,
+  ImgCmp,
+  EndDate,
+} from "./SubInfo";
+import DetailsDesc from "./DetailsDesc";
+import DetailsBid from "./DetailsBid";
 
-export { HomeHeader, NFTCard, FocusedStatusBar, CircleButton, RectButton };
+export {
+  HomeHeader,
+  NFTCard,
+  FocusedStatusBar,
+  CircleButton,
+  RectButton,
+  SubInfo,
+  EthPrice,
+  NFTTitle,
+  People,
+  ImgCmp,
+  EndDate,
+  DetailsDesc,
+  DetailsBid,
+};
--- screens/Details.js
+++ screens/Details.js
@@ -1,7 +1,27 @@
-import { Text } from "react-native";
+import {
+  Text,
+  View,
+  SafeAreaView,
+  Image,
+  StatusBar,
+  FlatList,
+} from "react-native";
 
-const Details = () => {
-  return <Text>Details</Text>;
+import { COLORS, SIZES, FONTS, SHADOWS, assets } from "../constants";
+import {
+  CircleButton,
+  RectButton,
+  SubInfo,
+  FocusedStatusBar,
+  DetailsDesc,
+  DetailsBid,
+} from "../components";
+const Details = ({ route, navigation }) => {
+  const { data } = route.params;
+  console.log("------------------");
+  console.log(data);
+  console.log("------------------");
+  return <Text>hello</Text>;
 };
 
 export default Details;
